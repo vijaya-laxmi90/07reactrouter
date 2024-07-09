e225e716343ab45f67e02f79f611ffcfa9d43603
Vijaya Laxmi <vijayalaxmi.02vl@gmail.com>
2024-07-09 22:48:57 +0530

    Initial commit

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState }  from "react";
+import { useLoaderData } from "react-router-dom";
+
+
+function Github(){
+    const data=useLoaderData()
+    // const [data,setData]=useState([])
+    // useEffect(()=>{
+    //    fetch('https://api.github.com/users/vijayalaxmi')
+    //    .then(response=>response.json())
+    //    .then (data=>{
+    //     console.log(data);
+    //     setData(data)
+    //    })
+    // },[])
+
+    return(
+        <div className='text-center m-4  bg-gray-600 text-white p-4 text-3xl'>Github Followers:{data.followers}
+        <img src="pic.png" alt="Git picture" width="300"/>
+        </div>
+    )
+}
+export default Github
+
+export const githubInfoLoader=async()=>{
+  const response=await  fetch('https://api.github.com/users/vijayalaxmi')
+  return response.json()
+}

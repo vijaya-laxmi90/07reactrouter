e48e9ed9300add4426cbb2a07caccd1f6313b92c
vijaya laxmi <159887996+vijaya-laxmi90@users.noreply.github.com>
2024-07-13 00:35:52 +0530

    Update Github.jsx

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -16,7 +16,7 @@ function Github(){
 
     return(
         <div className='text-center m-4  bg-gray-600 text-white p-4 text-3xl'>Github Followers:{data.followers}
-        <img src="pic.png" alt="Git picture" width="300"/>
+        <img src={pic.png} alt="Git picture" width="300"/>
         </div>
     )
 }
